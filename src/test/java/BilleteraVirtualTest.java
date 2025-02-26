be1003a7a8118422e5303ee7f2994c8d149672d8
vanessahg07 <vanehenaog05@gmail.com>
2025-02-26 10:49:36 -1100

    Método de suma test

--- src/test/java/BilleteraVirtualTest.java
+++ src/test/java/BilleteraVirtualTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class BilleteraVirtualTest {
@@ -24,7 +25,7 @@ public class BilleteraVirtualTest {
 
     @Test
     public void recargarSaldoTest() {
-        BilleteraVirtual billetera = new BilleteraVirtual(new Usuario("Carlos"));
+        BilleteraVirtual billetera = new BilleteraVirtual("1233291233", 10000, new ArrayList<>() , new Usuario("123465566",  "juan", "calle23", "juan@email.com", "12345", true));
         boolean resultado = billetera.recargarSaldo(1000);
 
         assertTrue(resultado);
@@ -33,22 +34,32 @@ public class BilleteraVirtualTest {
 
     @Test
     public void transferirTest() {
-        BilleteraVirtual origen = new BilleteraVirtual(new Usuario("Ana"));
-        BilleteraVirtual destino = new BilleteraVirtual(new Usuario("Luis"));
+
+        Usuario usuario1 = new Usuario("1234", "Diego", "calle 15 #3-22",  "diego@email.com", "12345", true);
+        Usuario usuario2 = new Usuario("12345", "Ana", "calle 17 #3-22",  "ana@email.com", "121212", true);
+
+
+        BilleteraVirtual origen = new BilleteraVirtual("12345", 300, new ArrayList<Transaccion>(), usuario1);
+        BilleteraVirtual destino = new BilleteraVirtual("4567", 200, new ArrayList<Transaccion>(), usuario2);
 
         origen.recargarSaldo(500);
         boolean resultado = origen.transferir(destino, 200, Categoria.VIAJES);
 
         assertTrue(resultado);
-        assertEquals(300, origen.consultarSaldo());
-        assertEquals(200, destino.consultarSaldo());
+        assertEquals(600, origen.consultarSaldo());
+        assertEquals(400, destino.consultarSaldo());
     }
 
     @Test
     public void consultarTransaccionesPorPeriodoTest() {
-        BilleteraVirtual billetera = new BilleteraVirtual(new Usuario("Paula"));
+
+        Usuario usuario1 = new Usuario("5678", "camilo", "calle 5 #4-32",  "camilo@email.com", "12345", true);
+        Usuario usuario2 = new Usuario("4678", "sofia", "calle 2 #4-32",  "sofia@email.com", "123456", true);
+
+        BilleteraVirtual billetera = new BilleteraVirtual("223344", 34523, new ArrayList<Transaccion>(), usuario1);
         billetera.recargarSaldo(1000);
-        billetera.transferir(new BilleteraVirtual(new Usuario("Diego")), 200, Categoria.FACTURAS);
+        BilleteraVirtual billeteraDesstino = new BilleteraVirtual("1111", 23234, new ArrayList<Transaccion>(), usuario2);
+        billetera.transferir(billeteraDesstino, 200, Categoria.VIAJES);
 
         LocalDateTime inicio = LocalDateTime.now().minusDays(1);
         LocalDateTime fin = LocalDateTime.now().plusDays(1);
@@ -58,14 +69,28 @@ public class BilleteraVirtualTest {
         assertFalse(transacciones.isEmpty());
     }
 
+
     @Test
     public void obtenerPorcentajeGastosIngresosTest() {
-        BilleteraVirtual billetera = new BilleteraVirtual(new Usuario("Sofia"));
-        billetera.recargarSaldo(1000);
-        billetera.transferir(new BilleteraVirtual(new Usuario("Diego")), 500, Categoria.VIAJES);
 
-        billetera.obtenerPorcentajeGastosIngresos(LocalDateTime.now().getYear(), LocalDateTime.now().getMonthValue());
+        Usuario usuario1 = new Usuario("5678", "felipe", "calle 5 #4-32",  "felipe@email.com", "12345", true);
+        Usuario usuario2 = new Usuario("4678", "miguel", "carrera 8 #4-32",  "miguel@email.com", "12345f", true);
+
+        BilleteraVirtual billeteraOrigen = new BilleteraVirtual("12341234", 600, new ArrayList<Transaccion>(), usuario1);
+        BilleteraVirtual billeteraDestino = new BilleteraVirtual("123412", 700, new ArrayList<Transaccion>(), usuario2);
+
+        billeteraOrigen.recargarSaldo(1000);
+        billeteraOrigen.transferir(billeteraDestino, 500, Categoria.VIAJES);
+
+
+
+        billeteraOrigen.obtenerPorcentajeGastosIngresos(LocalDateTime.now().getYear(), LocalDateTime.now().getMonthValue());
 
         // No hay assert porque el método imprime los resultados en la consola.
+    }
+
+    @Test
+    public void sumar(){
+
     }
 }
